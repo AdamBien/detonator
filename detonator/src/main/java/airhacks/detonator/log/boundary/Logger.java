4687719dae4eca8445c80bdd923aca95d70788ac
Adam Bien <abien@adam-bien.com>
2023-12-19 22:53:44 +0100

    terminal colors provided

--- detonator/src/main/java/airhacks/detonator/log/boundary/Logger.java
+++ detonator/src/main/java/airhacks/detonator/log/boundary/Logger.java
@@ -2,7 +2,28 @@ package airhacks.detonator.log.boundary;
 
 public interface Logger {
 
+    public enum TerminalColors {
+    
+        INFO("\u001B[34m"), TIME("\033[1;90m"), RESET("\u001B[0m");
+    
+        private final String value;
+    
+    
+        private TerminalColors(String value) {
+            this.value =  value;
+        }
+    
+        public String value() {
+            return this.value;
+        }
+    
+    }
+
     static void info(String info) {
+       System.out.println(info);
+    }
+
+    static void white(String info) {
        System.out.println(info);
     }
     
